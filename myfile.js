ccb9f5b6d7ed364118a39ca8f814e7ce0b1c7d2a
AkashVerma <akashverma@CLPT1930.local>
2024-01-18 10:37:04 +0530

    first commit

---
+++ myfile.js
@@ -0,0 +1,11 @@
+function left()
+{
+    const conent = document.querySelector('#content');
+    conent.scrollLeft -= 2000;
+}
+
+function right()
+{
+    const conent = document.querySelector('#content');
+    conent.scrollLeft += 2000;
+}
